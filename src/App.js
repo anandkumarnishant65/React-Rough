bf1855af20a966270bb22b51f4bb8835dbc24808
anandkumarnishant65 <anandkumar65@.com>
2024-01-17 19:56:16 +0530

    deleteIntoApp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState,useEffect} from 'react';
 import './App.css';
 import Student from './Components/Student';
-import StudentList from './Components/StudentList';
+// import StudentList from './Components/StudentList';
 
 function App() {
   
@@ -40,10 +40,27 @@ function App() {
     localStorage.setItem('list', JSON.stringify(studentsList))
   },[studentsList])
 
+  const deleteItem = (id) => {
+    let items = JSON.parse(localStorage.getItem('list'));
+    const filtered = items.filter(item => item.id !== id);
+    localStorage.setItem('list', JSON.stringify(filtered))
+    setStudentList(filtered)
+}
+
   return (
     <div className="App">
       <Student onAdd={studentListHandler}/>
-      <StudentList studs={studentsList}/>
+      {/* <StudentList studs={studentsList}/> */}
+
+      <ul>
+            {studentsList.map((user) => (
+                <li key={user.id}>
+                    {user.studentName} {user.studentMarks}
+                    <button onClick={()=>deleteItem(user.id)}>Delete</button>
+                </li>
+            ))}
+            
+        </ul>
     </div>
   );
 }
--- src/Components/StudentList.js
+++ src/Components/StudentList.js
@@ -1,28 +1,28 @@
-import React,{useState} from "react";
-import Card from "./Card";
+// import React from "react";
+// import Card from "./Card";
 
-const StudentList = (props) => {
-    
-    const deleteItem = (id) => {
-        let items = JSON.parse(localStorage.getItem('list'));
-        const filtered = items.filter(item => item.id !== id);
-        localStorage.setItem('list', JSON.stringify(filtered))
+// const StudentList = (props) => {
+
+//     const deleteItem = (id) => {
+//         let items = JSON.parse(localStorage.getItem('list'));
+//         const filtered = items.filter(item => item.id !== id);
+//         localStorage.setItem('list', JSON.stringify(filtered))
         
-    }
+//     }
 
-    return (
-        <Card>
-            <ul>
-            {props.studs.map((user) => (
-                <li key={user.id}>
-                    {user.studentName} {user.studentMarks}
-                    <button onClick={()=>deleteItem(user.id)}>Delete</button>
-                </li>
-            ))}
+//     return (
+//         <Card>
+//             <ul>
+//             {props.studs.map((user) => (
+//                 <li key={user.id}>
+//                     {user.studentName} {user.studentMarks}
+//                     <button onClick={()=>deleteItem(user.id)}>Delete</button>
+//                 </li>
+//             ))}
             
-        </ul>
-        </Card>
-    )
-}
+//         </ul>
+//         </Card>
+//     )
+// }
 
-export default StudentList
+// export default StudentList
